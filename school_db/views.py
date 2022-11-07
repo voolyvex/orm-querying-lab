82c533881c268597b207093ee025a2d359e770fd
voolyvex <1joeknapp@gmail.com>
2022-11-06 21:48:17 -0600

    setup

--- school_db/views.py
+++ school_db/views.py
@@ -59,7 +59,9 @@ SELECT `school_db_student`.`id`,
 # Print out each student's full name and gpa to the terminal
 def problem_one(request):
 
-    return complete(request)
+  print("hello worlds")
+
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
